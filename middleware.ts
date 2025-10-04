74fe08b0fea4643544dec21a8c46b0ca5c549df3
HyBiT-Dev <mr.jaturapat.d@gmail.com>
2025-10-05 06:52:49 +0700

    fix:config middleware block next images

--- middleware.ts
+++ middleware.ts
@@ -110,7 +110,7 @@ export const config = {
   // - Next.js internals
   // - Static files
   matcher: [
-    "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+    "/((?!api|trpc|_next|_next/image|favicon.ico|_vercel|.*\\..*).*)",
     // "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
     // Skip Next.js internals and all static files, unless found in search params
     // "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
