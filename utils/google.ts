0c6b96a5ab1c008690695ebfad9bf19b22ffbaab
HyBiT-Dev <mr.jaturapat.d@gmail.com>
2026-05-21 20:36:09 +0700

    fix(google): update googleModelPro to correct version "gemini-3.5-flash"

--- utils/google.ts
+++ utils/google.ts
@@ -14,7 +14,7 @@ const google = createVertex({
 
 const googleModel = "gemini-3-flash-preview";
 const googleModelLite = "gemini-3.1-flash-lite-preview";
-const googleModelPro = "gemini-3.1-pro-preview";
+const googleModelPro = "gemini-3.5-flash";
 const googleImage = "gemini-3.1-flash-image-preview";
 const googleImageModel = "gemini-2.5-flash-image";
 const googleImagePro = "gemini-3-pro-image-preview";
